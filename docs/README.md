4a62f9243d9c46b0baba415697ab164e31bb24bc
jolene <jolenetandev@gmail.com>
2026-08-20 15:35:06 +0800

    Rename main class and add greeting

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Mr Chatbot User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/MrChatbot.java
@@ -0,0 +1,18 @@
+public class MrChatbot {
+    public static void main(String[] args) {
+        String line = "____________________________________________________________";
+        String banner = "                       _           _   _           _   \n"
+                + " _ __ ___  _ __    ___| |__   __ _| |_| |__   ___ | |_ \n"
+                + "| '_ ` _ \\| '__|  / __| '_ \\ / _` | __| '_ \\ / _ \\| __|\n"
+                + "| | | | | | |    | (__| | | | (_| | |_| |_) | (_) | |_ \n"
+                + "|_| |_| |_|_|     \\___|_| |_|\\__,_|\\__|_.__/ \\___/ \\__|\n";
+
+        System.out.println(line);
+        System.out.println(banner);
+        System.out.println("Hello! I'm Mr Chatbot, your personal companion.");
+        System.out.println("What can I do for you?");
+        System.out.println(line);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(line);
+    }
+}

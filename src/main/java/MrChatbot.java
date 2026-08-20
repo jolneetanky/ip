50502c76127096acd6594c584e3f741cd28e992b
jolene <jolenetandev@gmail.com>
2026-08-20 15:46:00 +0800

    Implement echo

--- src/main/java/MrChatbot.java
+++ src/main/java/MrChatbot.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class MrChatbot {
     public static void main(String[] args) {
         String line = "____________________________________________________________";
@@ -12,7 +14,19 @@ public class MrChatbot {
         System.out.println("Hello! I'm Mr Chatbot, your personal companion.");
         System.out.println("What can I do for you?");
         System.out.println(line);
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(line);
+
+        // Read user inputs in a loop until they say "bye"
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNextLine()) {
+            String input = scanner.nextLine();
+            System.out.println(line);
+            if (input.equalsIgnoreCase("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println(line);
+                break;
+            }
+            System.out.println(input);
+            System.out.println(line);
+        }
     }
 }
